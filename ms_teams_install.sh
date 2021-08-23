e0aab2c5ea14735cd16af30afe3cd8fb01b1f89c
Manu Mathew <manukeerampanal@gmail.com>
2021-08-23 14:46:33 +0530

    Install MS Teams in centos 7
    Install MS Teams in centos 7. This should be used if MS Teams is not loading after installation in centos 7.

---
+++ ms_teams_install.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+GCC="gcc_7_5_0_release"
+BASE_DIR="ms_teams"
+MS_TEAMS_VERSION="1.4.00.13653"
+
+sudo yum -y remove teams
+
+sudo yum -y install svn texinfo-tex flex zip
+
+cd ~/Downloads/
+
+mkdir $BASE_DIR
+cd $BASE_DIR
+
+svn co "svn://gcc.gnu.org/svn/gcc/tags/$GCC/"
+
+cd $GCC
+
+./contrib/download_prerequisites
+
+./configure --disable-multilib --enable-languages=c,c++
+
+sudo make -j 8
+
+sudo make install
+
+gcc --version
+
+exit 0
+
+cd ..
+
+wget "https://packages.microsoft.com/yumrepos/ms-teams/teams-$MS_TEAMS_VERSION-1.x86_64.rpm"
+
+sudo yum -y install "./teams-$MS_TEAMS_VERSION-1.x86_64.rpm"
+
+sudo yum update
+
+teams
+
+
+
